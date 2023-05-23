a3bed17dddf62537822ad9d7262f71333a76539d
Bryan Goette <bryantgoette@hotmail.com>
2023-05-23 16:13:06 -0500

    add mdbootstrap to app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import App from "next/app";
 import { SSRProvider } from "@react-aria/ssr";
-import "bootstrap/dist/css/bootstrap.min.css";
+
+// CSS Libraries
+// import "bootstrap/dist/css/bootstrap.min.css";
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
 // Custom Components
 import PageLayout from "components/PageLayout";
 
