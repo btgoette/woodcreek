5a9e7c654f27a22660ad74aba8f2c41615cdc78f
Bryan Goette <bryantgoette@hotmail.com>
2023-06-23 13:36:38 -0500

    add call now button component and blocks for tile

--- components/FourBlockSection.jsx
+++ components/FourBlockSection.jsx
@@ -5,75 +5,74 @@ export default function FourBlockSection() {
     return (
         <div className="tiles">
             <Row className="justify-content-center align-items-center">
-                <Col className="tile p-2 d-flex align-items-center justify-content-center" md={{ span: 6, order: 0 }} xl={{ span: 3, order: 0 }} >
-                    <div className="tile-block">
-                        <div>
-                            <p className="tile-title">
+                <Col className="tile p-2" md={6} xl={4}>
+                    <Image className="w-100" src="/images/500x500/kitchen-tile-1.png" alt="" />
+                </Col>
+                <Col className="tile p-2 d-flex align-items-center justify-content-center" md={6} xl={4} >
+                    <div className="bg-primary d-flex align-items-center justify-content-center text-center w-100 ratio ratio-1x1">
+                        <div className="p-4 position-relative">
+                            <h2 className="my-0">1.</h2>
+                            <p className="fs-3">
                                 <strong>
-                                    1. Pre-treating with Heavy-Duty Grout Cleaner:
+                                    Pre-treating
                                 </strong>
                             </p>
-                            <p className="tile-content">
+                            <hr />
+                            <p className="text-start">
                                 We begin by applying a powerful grout cleaner to effectively treat the tile and grout areas. This specially formulated cleaner is designed to break down stubborn soil and residue.
                             </p>
                         </div>
                     </div>
                 </Col>
-                <Col className="tile p-2" md={{ span: 6, order: 1 }} xl={{ span: 3, order: 1 }}>
-                    <Image className="w-100" src="/images/500x500/kitchen-tile-1.png" alt="" />
-                </Col>
-                <Col className="tile p-2 d-flex align-items-center justify-content-center" md={{ span: 6, order: 3 }} xl={{ span: 3, order: 2 }}>
-                    <div className="tile-block">
-                        <div>
-                            <p className="tile-title">
+                <Col className="tile p-2 d-flex align-items-center justify-content-center" md={6} xl={4}>
+                    <div className="bg-primary d-flex align-items-center justify-content-center text-center w-100 ratio ratio-1x1">
+                        <div className="p-4 position-relative">
+                            <h2 className="my-0">2.</h2>
+                            <p className="fs-3">
                                 <strong>
-                                    2. Hand-Scrubbing for Thorough Cleaning:
+                                    Hand-Scrubbing
                                 </strong>
                             </p>
-                            <p className="tile-content">
+                            <hr />
+                            <p className="text-start">
                                 Our skilled technicians meticulously hand-scrub the area, thoroughly cleaning every inch and leaving your tile and grout looking refreshed.
                             </p>
                         </div>
                     </div>
                 </Col>
-                <Col className="tile p-2" md={{ span: 6, order: 2 }} xl={{ span: 3, order: 3 }}>
-                    <Image className="w-100" src="/images/500x500/kitchen-tile-2.png" alt="" />
-                </Col>
-            </Row>
-            <Row className="justify-content-center align-items-center">
-                <Col className="tile p-2 d-flex align-items-center justify-content-center" md={{ span: 6, order: 0 }} xl={{ span: 3, order: 1 }}>
-                    <div className="tile-block">
-                        <div>
-                            <p className="tile-title">
+                <Col className="tile p-2 d-flex align-items-center justify-content-center" md={6} xl={4} >
+                    <div className="bg-primary d-flex align-items-center justify-content-center text-center w-100 ratio ratio-1x1">
+                        <div className="p-4 position-relative">
+                            <h2 className="my-0">3.</h2>
+                            <p className="fs-3">
                                 <strong>
-                                    3. Pressure-Cleaning for Deep Cleaning:
+                                    Pressure-Cleaning
                                 </strong>
                             </p>
-                            <p className="tile-content">
+                            <hr />
+                            <p className="text-start">
                                 We use advanced pressure-cleaning equipment for a deep and thorough tile and grout cleaning, effectively removing embedded dirt and grime, revitalizing your surfaces.
                             </p>
                         </div>
                     </div>
                 </Col>
-                <Col className="tile p-2" md={{ span: 6, order: 1 }} xl={{ span: 3, order: 0 }}>
-                    <Image className="w-100" src="/images/500x500/kitchen-tile-3.png" alt="" />
-                </Col>
-                <Col className="tile p-2 d-flex align-items-center justify-content-center" md={{ span: 6, order: 3 }} xl={{ span: 3, order: 3 }}>
-
-                    <div className="tile-block">
-                        <div>
-                            <p className="tile-title">
+                <Col className="tile p-2 d-flex align-items-center justify-content-center" md={6} xl={4}>
+                    <div className="bg-primary d-flex align-items-center justify-content-center text-center w-100 ratio ratio-1x1">
+                    <div className="p-4 position-relative">
+                            <h2 className="my-0">4.</h2>
+                            <p className="fs-3">
                                 <strong>
-                                    4. Recommended Tile Sealing:
+                                    Tile Sealing
                                 </strong>
                             </p>
-                            <p className="tile-content">
-                                After cleaning, we recommend professional-grade tile sealing to protect against permanent stains and damage. Our expert team applies an invisible barrier sealant for long-lasting protection.
+                            <hr/>
+                            <p className="text-start">
+                                After cleaning, we recommend professional-grade tile sealing to protect against stains and damage. Our expert team applies an invisible barrier sealant for long-lasting protection.
                             </p>
                         </div>
                     </div>
                 </Col>
-                <Col className="tile p-2" md={{ span: 6, order: 2 }} xl={{ span: 3, order: 2 }}>
+                <Col className="tile p-2" md={6} xl={4}>
                     <Image className="w-100" src="/images/500x500/kitchen-tile-6.png" alt="" />
                 </Col>
             </Row>
