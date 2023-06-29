f2fe18769f9d2fd087034a3946c29766b5c2c6e1
Bryan Goette <bryantgoette@hotmail.com>
2023-06-29 17:41:22 -0500

    re-organize paragraphs and remove fluff SEO end paragraphs

--- pages/carpet-cleaning/index.jsx
+++ pages/carpet-cleaning/index.jsx
@@ -10,22 +10,20 @@ let hero = {
 
 export default function CarpetCleaning() {
   return (
-    <div>
+    <>
+
       <Hero {...hero} />
+      
       <section className="py-5">
         <Container className="p-5">
           <Row className="align-items-center">
-            <Col xs={12}>
-              <h2>
+            <Col className="mb-2 mb-lg-0" lg={5}>
+              <Image className="w-100" src="/images/500x400/wine-pexels.webp" alt="red wine" />
+            </Col>
+            <Col lg={7}>
+              <h2 className="mt-lg-0">
                 Easily Eliminate Tough Stains with Professional Carpet Cleaning Services
               </h2>
-            </Col>
-            <Col lg={{ span: 6, order: 0 }} xl={4}>
-              <figure className="mb-xl-0">
-                <Image className="w-100" src="/images/500x400/wine-pexels.webp" alt="red wine" />
-              </figure>
-            </Col>
-            <Col lg={6} xl={8}>
               <p>
                 Say goodbye to stubborn stains with the help of Woodcreek Carpet Tech&apos;s professional carpet cleaning services. Whether it&apos;s a Monday morning coffee spill or other challenging stains like food, pet accidents, dirt-tracked shoes, or even red wine and mustard, our expert team is here to assist you.
               </p>
@@ -43,48 +41,50 @@ export default function CarpetCleaning() {
       <section className="bg-carpet py-5">
         <Container className="p-5">
           <Row className="align-items-center">
-            <Col xs={12}>
-              <h2>
+            <Col>
+              <h2 className="mt-lg-0">
                 Choose Woodcreek Carpet Tech for Exceptional Carpet Cleaning
               </h2>
               <p>
                 Looking for the best carpet cleaning service? Look no further than Woodcreek Carpet Tech! We go above and beyond to exceed your expectations and deliver outstanding results. Here&apos;s why you should choose us:
               </p>
             </Col>
-            <Col lg={{ span: 6, order: 0 }} xl={4}>
-              <figure className="mb-xl-0">
-                <Image className="w-100" src="/images/500x400/carpet-cleaning-midjourney.webp" alt="coffee spilled on carpet" />
-              </figure>
+          </Row>
+          <Row>
+            <Col className="mb-2 mb-lg-0" lg={{ span: 5, order: 1 }}>
+              <Image className="w-100" src="/images/500x400/carpet-cleaning-midjourney.webp" alt="cleaning carpet" />
             </Col>
-            <Col lg={6} xl={8}>
+            <Col lg={7}>
               <ul>
-                <li><strong>Experienced and Certified Technicians</strong>: Our team of certified technicians is highly trained and brings years of experience to every job. You can trust their expertise to handle your carpets with care and precision.</li>
-                <li><strong>Powerful Truck-Mounted Machines</strong>: We utilize the most powerful truck-mounted machines available, ensuring deep cleaning and maximum extraction of dirt, stains, and allergens from your carpets. Our advanced equipment guarantees exceptional results.</li>
-                <li><strong>Latest Cleaning Solutions</strong>: We stay up-to-date with the latest advancements in the industry and use cutting-edge cleaning solutions to tackle even the toughest stains and odors. Rest assured that we will leave your carpets looking and smelling fresh.</li>
-                <li><strong>Uncompromising Commitment to Cleanliness</strong>: At Woodcreek Carpet Tech, we prioritize cleanliness, presentation, and the health of your home. We go the extra mile to ensure a thorough cleaning that leaves your carpets not only visually appealing but also hygienic for you and your family.</li>
+                <li className="mb-3">
+                  <strong>Experienced and Certified Technicians</strong>: Our team of certified technicians is highly trained and brings years of experience to every job. You can trust their expertise to handle your carpets with care and precision.</li>
+
+                <li className="mb-3"><strong>Powerful Truck-Mounted Machines</strong>: We utilize the most powerful truck-mounted machines available, ensuring deep cleaning and maximum extraction of dirt, stains, and allergens from your carpets. Our advanced equipment guarantees exceptional results.</li>
+
+                <li className="mb-3"><strong>Latest Cleaning Solutions</strong>: We stay up-to-date with the latest advancements in the industry and use cutting-edge cleaning solutions to tackle even the toughest stains and odors. Rest assured that we will leave your carpets looking and smelling fresh.</li>
+
+                <li className="mb-3"><strong>Uncompromising Commitment to Cleanliness</strong>: At Woodcreek Carpet Tech, we prioritize cleanliness, presentation, and the health of your home. We go the extra mile to ensure a thorough cleaning that leaves your carpets not only visually appealing but also hygienic for you and your family.</li>
               </ul>
             </Col>
+          </Row>
+          <Row>
             <Col className="mt-4">
               <p>Trust Woodcreek Carpet Tech for all your carpet cleaning needs. Experience our dedication to exceeding your standards and providing you with a clean, presentable, and healthy home. Contact us today to schedule an appointment and discover the difference we can make!</p>
             </Col>
           </Row>
         </Container>
       </section>
-      <section className="py-5">
 
+      <section className="py-5">
         <Container className="p-5">
           <Row className="align-items-center">
-            <Col xs={12}>
-              <h2>
+            <Col className="mb-2 mb-lg-0" lg={{ span: 5, order: 1 }}>
+                <Image className="w-100" src="/images/500x400/clean-carpet-pexels.webp" alt="clean carpet with fruit" />
+            </Col>
+            <Col lg={7}>
+              <h2 className="mt-lg-0">
                 Your Trusted Carpet Cleaning Solution in Roseville, CA!
               </h2>
-            </Col>
-            <Col lg={{ span: 6, order: 1 }} xl={4}>
-              <figure className="mb-xl-0">
-                <Image className="w-100" src="/images/500x400/clean-carpet-pexels.webp" alt="clean carpet with fruit" />
-              </figure>
-            </Col>
-            <Col lg={6} xl={8}>
               <p>
                 Looking for a reliable and professional carpet cleaning company in Roseville, CA? Look no further than Woodcreek Carpet Tech! With years of experience and a dedication to excellence, we are the go-to experts for transforming your carpets into pristine works of art.
               </p>
@@ -94,14 +94,11 @@ export default function CarpetCleaning() {
               <p>
                 Whether you have a residential or commercial space, our tailored cleaning approach ensures a thorough and meticulous service. Say goodbye to allergens and hello to fresh, clean carpets that enhance the overall aesthetics of your environment.
               </p>
-              <p>
-                At Woodcreek Carpet Tech, we take pride in our professionalism, reliability, and commitment to customer satisfaction. You can trust us to deliver exceptional results and make your carpets look and feel their best. Choose Woodcreek Carpet Tech for all your carpet cleaning needs in Roseville, CA. Contact us today to schedule an appointment and experience the difference we can make!
-              </p>
             </Col>
           </Row>
         </Container>
       </section>
-      {/* <FAQ /> */}
-    </div>
+
+    </>
   );
 }
--- pages/reviews/index.jsx
+++ pages/reviews/index.jsx
@@ -12,18 +12,14 @@ export default function Reviews() {
   return (
     <div>
       <Hero {...hero} />
-      <section className="pt-5">
-        <Container className="px-5">
-          <Row>
+      <section className="p-5">
+        <Container className="py-5">
+          <Row className="pb-5">
             <Col>
-              <h2 className="text-center">Welcome to our Reviews Page - Where Customer Satisfaction Comes First!</h2>
+              <h2>Welcome to our Reviews Page - Where Customer Satisfaction Comes First!</h2>
               <p>At Woodcreek Carpet Tech, we take great pride in providing exceptional service and exceeding our customers&apos; expectations. We believe that our customers&apos; feedback is invaluable in showcasing the quality of our work and the satisfaction of our clients. On this Reviews Page, you&apos;ll find testimonials and reviews from our valued customers who have experienced firsthand the professionalism, reliability, and outstanding results we deliver. Take a moment to read through their experiences and see why Woodcreek Carpet Tech is the trusted choice for all your carpet cleaning and maintenance needs. Your satisfaction is our priority, and we look forward to adding you to our list of happy customers.</p>
             </Col>
           </Row>
-        </Container>
-      </section>
-      <section className="pb-5">
-        <Container className="py-5">
           <Review />
         </Container>
       </section>
